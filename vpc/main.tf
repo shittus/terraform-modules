82df454507cde884afce75a127d92bcd444e1e36
shittus <97743642+shittus@users.noreply.github.com>
2024-10-10 22:04:42 -0500

    Update main.tf

--- vpc/main.tf
+++ vpc/main.tf
@@ -26,7 +26,7 @@ resource "aws_subnet" "public_subnet_az1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_az1_cidr
 
-  availability_zone       = aws_availability_zones.available_zones.names[0]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
 
   map_public_ip_on_launch = true
 
@@ -40,7 +40,7 @@ resource "aws_subnet" "public_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.public_subnet_az2_cidr
 
-  availability_zone       = aws_availability_zones.available_zones.names[0]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
 
   map_public_ip_on_launch = true
 
@@ -79,8 +79,7 @@ resource "aws_route_table_association" "public_subnet_2_rt_association" {
 resource "aws_subnet" "private_app_subnet_az1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_app_subnet_az1_cidr
-
-  availability_zone       = aws_availability_zones.available_zones.names[0]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
 
   map_public_ip_on_launch = false
 
@@ -93,8 +92,7 @@ resource "aws_subnet" "private_app_subnet_az1" {
 resource "aws_subnet" "private_app_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_app_subnet_az2_cidr
-
-  availability_zone       = aws_availability_zones.available_zones.names[1]
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
 
   map_public_ip_on_launch = false
 
@@ -107,8 +105,7 @@ resource "aws_subnet" "private_app_subnet_az2" {
 resource "aws_subnet" "private_data_subnet_az1" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_data_subnet_az1_cidr
-
-  availability_zone       = aws_availability_zones.available_zones.names[0]
+  availability_zone       = data.aws_availability_zones.available_zones.names[0]
 
 
   map_public_ip_on_launch = false
@@ -122,8 +119,7 @@ resource "aws_subnet" "private_data_subnet_az1" {
 resource "aws_subnet" "private_data_subnet_az2" {
   vpc_id                  = aws_vpc.vpc.id
   cidr_block              = var.private_data_subnet_az2_cidr
-
-  availability_zone       = aws_availability_zones.available_zones.names[1]
+  availability_zone       = data.aws_availability_zones.available_zones.names[1]
 
   map_public_ip_on_launch = false
 
