f4d56b355fb0361207ce3a69b1dc941d8f8be2b8
soliu <soliu@yahoo.com>
2024-10-14 16:13:53 -0500

    updated

--- acm/main.tf
+++ acm/main.tf
@@ -40,45 +40,38 @@
 # }
 
 
-# Data Source for ACM Certificate
-# Fetch details of an existing ACM certificate
-data "aws_acm_certificate" "existing_cert" {
-  domain   = var.domain_name
-  statuses = ["ISSUED"]  # Fetch an issued certificate
+
+
+# Create the ACM certificate
+resource "aws_acm_certificate" "cert" {
+  domain_name       = var.domain_name
+  validation_method = "DNS"
 }
 
-# Get details about the Route 53 hosted zone
-data "aws_route53_zone" "route53_zone" {
-  name         = var.domain_name
-  private_zone = false
-}
-
-# Route 53 DNS record for ACM certificate validation
+# Create Route 53 DNS validation records
 resource "aws_route53_record" "acm_validation" {
   for_each = {
-    for dvo in data.aws_acm_certificate.existing_cert.domain_validation_options : dvo.domain_name => {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => {
       name   = dvo.resource_record_name
       type   = dvo.resource_record_type
       value  = dvo.resource_record_value
     }
   }
 
+
+  zone_id        = data.aws_route53_zone.route53_zone.zone_id
+  name           = each.value.name
+  type           = each.value.type
+  ttl            = 60
+  records        = [each.value.value]
   allow_overwrite = true
-  zone_id = data.aws_route53_zone.route53_zone.zone_id  # Correct reference
-  name    = ws_acm_certificate_validation.cert_validation.namd
-  type    = "CNAME"
-  ttl     = 60
-  records = [aws_acm_certificate_validation.cert_validation.id]
 }
 
-# # Wait for ACM certificate validation to complete
-# resource "aws_acm_certificate_validation" "cert_validation" {
-#   certificate_arn         = data.aws_acm_certificate.existing_cert.arn
-#   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
-# }
 
+# ACM certificate validation after DNS records are created
 resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = data.aws_acm_certificate.existing_cert.arn
-  validation_record_fqdns = [aws_route53_record.acm_validation.fqdn]
-}
+  certificate_arn         = aws_acm_certificate.cert.arn
+  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 
+  depends_on = [aws_route53_record.acm_validation]  # Ensures validation waits for DNS records
+}
