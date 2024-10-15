c014467d09eab4d41f1aed81194fa62fdd96bd46
soliu <soliu@yahoo.com>
2024-10-14 22:59:59 -0500

    iam-role-updated

--- iam-role/output.tf
+++ iam-role/output.tf
@@ -1,4 +1,5 @@
 # export the ecs task execution role arn 
 output "ecs_task_execution_role_arn" {
-  value = aws_iam_role.cs_task_execution_role.arn
+  value = aws_iam_role.ecs_task_execution_role.arn
 }
+
